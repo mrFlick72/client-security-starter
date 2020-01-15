bac2ef0e3f1b9cb870935766cd9023e283b35657
mrFlick72 <valerio.vaudi@gmail.com>
2020-01-15 19:18:19 +0100

    global front channel logout endpoint for blocking and no blocking io web platform

--- src/main/java/it/valeriovaudi/vauthenticator/security/clientsecuritystarter/logout/OIDCReactiveGlobalFrontChannelLogout.java
+++ src/main/java/it/valeriovaudi/vauthenticator/security/clientsecuritystarter/logout/OIDCReactiveGlobalFrontChannelLogout.java
@@ -1,27 +1,29 @@
 package it.valeriovaudi.vauthenticator.security.clientsecuritystarter.logout;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.cloud.client.ConditionalOnReactiveDiscoveryEnabled;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.result.view.Rendering;
 
 @Controller
-@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-public class OIDCGlobalFrontChannelLogout {
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+public class OIDCReactiveGlobalFrontChannelLogout {
 
     private final GlobalFrontChannelLogoutProvider globalFrontChannelLogoutProvider;
 
-    public OIDCGlobalFrontChannelLogout(GlobalFrontChannelLogoutProvider globalFrontChannelLogoutProvider) {
+    public OIDCReactiveGlobalFrontChannelLogout(GlobalFrontChannelLogoutProvider globalFrontChannelLogoutProvider) {
         this.globalFrontChannelLogoutProvider = globalFrontChannelLogoutProvider;
     }
 
     @GetMapping(value = "/oidc_logout.html")
-    public String logout(Model model, OAuth2AuthenticationToken authentication) {
+    public Rendering logout(OAuth2AuthenticationToken authentication) {
         OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
         String logoutUrl = globalFrontChannelLogoutProvider.getLogoutUrl(oidcUser.getIdToken());
-        model.addAttribute("logoutUrl", logoutUrl);
-        return "redirect:" + logoutUrl;
+
+        return Rendering.redirectTo(logoutUrl).modelAttribute("logoutUrl", logoutUrl).build() ;
     }
 }
