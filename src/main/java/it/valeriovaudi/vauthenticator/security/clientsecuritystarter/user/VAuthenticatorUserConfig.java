b042dae37725b6282067da28dd16a7867dc309eb
mrFlick72 <valerio.vaudi@gmail.com>
2020-03-20 00:56:08 +0100

    user name field

--- src/main/java/it/valeriovaudi/vauthenticator/security/clientsecuritystarter/user/VAuthenticatorUserConfig.java
+++ src/main/java/it/valeriovaudi/vauthenticator/security/clientsecuritystarter/user/VAuthenticatorUserConfig.java
@@ -1,19 +1,13 @@
 package it.valeriovaudi.vauthenticator.security.clientsecuritystarter.user;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class VAuthenticatorUserConfig {
 
     @Bean
-    public VAuthenticatorUserNameResolver vAuthenticatorUserNameResolver(@Value("${spring.security.oauth2.client.provider.vauthenticator.user-name-attribute:username}") String userNameSource) {
-        return new VAuthenticatorUserNameResolver(userNameSource);
+    public VAuthenticatorUserNameResolver vAuthenticatorUserNameResolver() {
+        return new VAuthenticatorUserNameResolver("username");
     }
 }
