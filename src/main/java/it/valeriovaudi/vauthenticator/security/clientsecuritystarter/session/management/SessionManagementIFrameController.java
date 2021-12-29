fbfa4a52f192957fd93998dbb54732cfbfaf3a0e
mrflick72 <valerio.vaudi@gmail.com>
2021-12-29 19:19:48 +0100

    client oidc session management capabilities

--- src/main/java/it/valeriovaudi/vauthenticator/security/clientsecuritystarter/session/management/SessionManagementIFrameController.java
+++ src/main/java/it/valeriovaudi/vauthenticator/security/clientsecuritystarter/session/management/SessionManagementIFrameController.java
@@ -7,25 +7,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.Duration;
 
 @Controller
 public class SessionManagementIFrameController {
 
     private final String targetOrigin;
     private final String logoutUri;
+    private final Duration pollingRate;
 
     public SessionManagementIFrameController(
             @Value("${vauthenticator.session-management.rp-iframe.origin}") String targetOrigin,
-            @Value("${vauthenticator.session-management.rp-iframe.logout-uri}") String logoutUri
+            @Value("${vauthenticator.session-management.rp-iframe.logout-uri}") String logoutUri,
+            @Value("${vauthenticator.session-management.rp-iframe.polling-rate}") Duration pollingRate
     ) {
         this.targetOrigin = targetOrigin;
         this.logoutUri = logoutUri;
+        this.pollingRate = pollingRate;
     }
 
     @GetMapping("/session/management")
     public String sessionManagerIframe(OAuth2AuthenticationToken principal, Model model, HttpSession session) {
         model.addAttribute("logout_uri", logoutUri);
         model.addAttribute("target_origin", targetOrigin);
+        model.addAttribute("polling_rate", pollingRate.toMillis());
         model.addAttribute("client_id", principal.getAuthorizedClientRegistrationId());
         model.addAttribute("session_state", session.getAttribute("op.session_state"));
         return "session/management";
